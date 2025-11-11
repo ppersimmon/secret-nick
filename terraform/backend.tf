d598cb3a6d50dc016f4f455e8a6fe998ace86195
Maria <mary.chemikos@gmail.com>
2025-11-11 10:03:07 +0200

    fix: Correct bucket name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform-tfstate-jkbskefbjek"
+    bucket       = "secret-nick-santa-terraform-2025marathon"
     key          = "terraform.tfstate"
     region       = "eu-central-1"
     use_lockfile = true
